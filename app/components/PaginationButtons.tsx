dbcaabd0b97a906570edee4a391f0d87da1cfb31
Darick <darickmcbride@me.com>
2024-02-17 14:20:47 -0800

    added suspense boundaries

--- app/components/PaginationButtons.tsx
+++ app/components/PaginationButtons.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname, useSearchParams } from "next/navigation";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { Suspense } from "react";
 
 export default function PaginationButtons() {
   const pathname = usePathname();
@@ -10,30 +11,34 @@ export default function PaginationButtons() {
   const searchTerm = searchParams.get("searchTerm");
   const startIndex = +(searchParams.get("start") || 1);
 
-
-
   return (
     <div className="text-secondary flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
-      {startIndex >= 10 && (
-        <Link
-          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex - 10}`}
-        >
-          <div className="flex flex-col items-center hover:underline">
-            <BsChevronLeft className="h-5" />
-            <p>Previous</p>
-          </div>
-        </Link>
-      )}
-      {startIndex <= 90 && (
-        <Link
-          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex + 10}`}
-        >
-          <div className="flex flex-col items-center hover:underline">
-            <BsChevronRight className="h-5" />
-            <p>Next</p>
-          </div>
-        </Link>
-      )}
+      <Suspense fallback="...Loading">
+        {startIndex >= 10 && (
+          <Link
+            href={`${pathname}?searchTerm=${searchTerm}&start=${
+              startIndex - 10
+            }`}
+          >
+            <div className="flex flex-col items-center hover:underline">
+              <BsChevronLeft className="h-5" />
+              <p>Previous</p>
+            </div>
+          </Link>
+        )}
+        {startIndex <= 90 && (
+          <Link
+            href={`${pathname}?searchTerm=${searchTerm}&start=${
+              startIndex + 10
+            }`}
+          >
+            <div className="flex flex-col items-center hover:underline">
+              <BsChevronRight className="h-5" />
+              <p>Next</p>
+            </div>
+          </Link>
+        )}
+      </Suspense>
     </div>
   );
 }
